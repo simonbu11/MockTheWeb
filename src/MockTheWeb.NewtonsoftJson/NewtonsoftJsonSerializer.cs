0ad48fe1472d2d0f55d284552b100038567994ec
Simon Bull <simon.bull.84@googlemail.com>
2020-12-15 10:58:08 +0000

    Add package to serialize json using Json.NET

---
+++ src/MockTheWeb.NewtonsoftJson/NewtonsoftJsonSerializer.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+
+namespace MockTheWeb.NewtonsoftJson
+{
+    public class NewtonsoftJsonSerializer : IJsonSerializer
+    {
+
+        public NewtonsoftJsonSerializer()
+        {
+        }
+        public NewtonsoftJsonSerializer(JsonSerializerSettings serializerSettings)
+        {
+            SerializerSettings = serializerSettings;
+        }
+        
+        public JsonSerializerSettings SerializerSettings { get; }
+        
+        public string Serialize(object item)
+        {
+            if (SerializerSettings != null)
+            {
+                return JsonConvert.SerializeObject(item, SerializerSettings);
+            }
+            
+            return JsonConvert.SerializeObject(item);
+        }
+    }
+}
